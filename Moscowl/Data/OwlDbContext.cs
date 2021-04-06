60364845b6d4571a32946cfdb2be408704dc51f3
Kivenkantaja Kivenkantaja <bushuevdv@mail.ru>
2021-04-06 22:57:14 +0300

    add db context

---
+++ Moscowl/Data/OwlDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moscowl.Models;
+
+namespace Moscowl.Data
+{
+    public class OwlDbContext : DbContext
+    {
+        public OwlDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
