9f849f89a9ef64231dcaead4cccad4340a57898f
N1ghtStorm <bushuevdv@mail.ru>
2021-04-07 15:23:45 +0300

    add random secret gen

--- Moscowl/Services/Extensions/ServiceExtensions.cs
+++ Moscowl/Services/Extensions/ServiceExtensions.cs
@@ -2,6 +2,8 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using Moscowl.Config;
+using System;
+using System.Linq;
 using System.Text;
 
 namespace Moscowl.Services.Extensions
@@ -27,16 +29,22 @@ namespace Moscowl.Services.Extensions
                     options.TokenValidationParameters = new TokenValidationParameters
                     {
                         ValidateIssuerSigningKey = true,
+                        ValidateLifetime = true,
                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secret)),
                         ValidateIssuer = false,
-                        ValidateAudience = false
+                        ValidateAudience = false,
+                        ClockSkew = TimeSpan.Zero
                     };
                 });
         }
 
         private static string GetRandomSecret()
         {
-            return "asdasd3d3d3dQASDASRQWERT3WQEER2";
+            var random = new Random();
+            var random_num_len = random.Next(30, 50);
+            var chars = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890!@#$%^&*+_=";
+            var secret = new string(Enumerable.Repeat(chars, random_num_len).Select(x => x[random.Next(x.Length)]).ToArray());
+            return secret;
         }
     }
 }
