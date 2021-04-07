ce159beb20fdaf88e8c72a59bdf8fa0def6687e5
Kivenkantaja Kivenkantaja <bushuevdv@mail.ru>
2021-04-07 22:47:31 +0300

    add player season implementaton

---
+++ Moscowl/Repositories/PlayerRepository.cs
@@ -0,0 +1,35 @@
+﻿using Moscowl.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Moscowl.Repositories
+{
+    public class PlayerRepository : IPlayerRepository
+    {
+        public Task<Player> CreatePlayer(Player player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Player> DeletePlayer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Player> GetPlayer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Player>> GetPlayers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Player> UpdatePlayer(int id, Player player)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
